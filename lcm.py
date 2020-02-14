44188f42b364663ece45867278639199d07f8955
poojadjiva <61045261+poojadjiva@users.noreply.github.com>
2020-02-13 21:25:19 -1200

    Add files via upload

---
+++ lcm.py
@@ -0,0 +1,14 @@
+def compute_lcm(x,y):
+    if(x>y):
+        greater=x
+    else:
+        greater=y
+    while True:
+        if(greater%x==0)and(greater%y==0):
+            lcm=greater
+            break
+        greater+=1
+    return lcm
+x=int(input())
+y=int(input())
+print("the lcm is", compute_lcm(x, y))
